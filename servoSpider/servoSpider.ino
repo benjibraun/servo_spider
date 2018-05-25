f537fb108866bea34227b7eef4aa7a14937749c0
benjibraun <benjibraun@gmail.com>
2018-05-26 01:38:02 +0200

    servoSpider.srvo_spider.h

--- servoSpider/servoSpider.ino
+++ servoSpider/servoSpider.ino
@@ -1,9 +1,16 @@
 #include <Servo.h>
-#include "
+#include "servo_spider.h"
+int servo_pins [12] = {2,3,4,5,6,7,8,9,10,11,13};
+Servo servo1;
 void setup() {
   Serial.begin(9600);
-  myservo.attach(9)
-
+  for (int i = 2; i < 14; i++){ 
+  servo1.attach(i);
+  }
+  Serial.println(" ||  ||");
+  Serial.println(" \\()//");
+  Serial.println("//(__)\\");
+  Serial.println("||    ||");
 }
 
 void loop() {
