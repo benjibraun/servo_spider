766d19b7506286a8a98560944eafc63768f296cd
benjibraun <benjibraun@gmail.com>
2018-06-05 23:58:01 +0200

    servo numbering test

--- servoSpider/servoSpider.ino
+++ servoSpider/servoSpider.ino
@@ -2,10 +2,23 @@
 #include "servo_spider.h"
 int servo_pins [12] = {2,3,4,5,6,7,8,9,10,11,13};
 Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+Servo servo6;
+Servo servo7;
+Servo servo8;
+Servo servo9;
+Servo servo10;
+Servo servo11;
+Servo servo12;
+Servo servos[12]={servo1, servo2,servo3,servo4,servo5,servo6,servo7,servo8,servo9,servo10,servo11,servo12};
+int command =90 ;
 void setup() {
-  Serial.begin(9600);
-  for (int i = 2; i < 14; i++){ 
-  servo1.attach(i);
+  Serial.begin(115200);
+  for (int i = 0; i < 12; i++){ 
+  servos[i].attach(servo_pins [i]);
   }
   Serial.println(" ||  ||");
   Serial.println(" \\()//");
@@ -15,4 +28,30 @@ void setup() {
 
 void loop() {
 
+  for (int i = 0; i < 12; i++)
+  {
+    
+    servos[i].write(command);
+    Serial.println("servo :" + (String) i + "=" + (String) command);
+    if (Serial.available())
+    {
+      //command = Serial.readString();
+      command = Serial.parseInt();
+      digitalWrite((int)command,HIGH);
+    }
+    delay(1000);
+
+    
+    /*
+    servos[i].write(0);
+    Serial.println("servo :" + (String)i + "=" + "0");
+    delay(100);
+    servos[i].write(90);
+    Serial.println("servo :" + (String)i + "=" + "90");
+    delay(100);
+    servos[i].write(45);
+    Serial.println("servo :" + (String)i + "=" + "45");
+    delay(100);
+    */
+  }
 }
---
+++ servoSpider/servo_spider.h
@@ -0,0 +1,7 @@
+#define UP_SERVO_MAX 45
+#define UP_SERVO_MIN 0
+#define MID_SERVO_MAX 45
+#define MID_SERVO_MIN 0
+#define DOW_SERVO_MAX 45
+#define DOW_SERVO_MIN 0
+
